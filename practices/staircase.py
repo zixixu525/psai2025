5b042ad3af7f0f8750b7c82b667663fbe06b7601
zixi xu <serena@zixis-MacBook-Pro.local>
2025-07-01 08:50:53 -0400

    Add staircase.py with main function

---
+++ practices/staircase.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    n=int(input("enter staircase height"))
+    for i in range(n,0,-1):
+        print( '#' *i)
+
+if __name__ == "__main__":
+    main()
+    
+    
+    
+    
+
+         
